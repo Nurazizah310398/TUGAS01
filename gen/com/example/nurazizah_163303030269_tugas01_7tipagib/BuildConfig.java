3962021ae59b316807ccd69afeb706c2f1556b5e
Nurazizah310398 <zeea542@gmail.com>
2019-11-28 19:26:06 +0700

    Tugas 1

---
+++ gen/com/example/nurazizah_163303030269_tugas01_7tipagib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.nurazizah_163303030269_tugas01_7tipagib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
